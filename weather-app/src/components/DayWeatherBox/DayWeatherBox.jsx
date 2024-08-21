5d4cf0fdc7083f1e6cbee8100a96dc7f3dcb37f5
bbiomax <bbbiomax@gmail.com>
2024-08-21 14:22:37 +0300

    небольшой адаптив

--- weather-app/src/components/DayWeatherBox/DayWeatherBox.jsx
+++ weather-app/src/components/DayWeatherBox/DayWeatherBox.jsx
@@ -18,8 +18,24 @@ export function DayWeatherBox({ currentWeather }) {
         </div>
 
         <div className={styles.windAndHumidity}>
-          <span>Скорость ветра: {(currentWeather.wind.speed).toFixed(1)} м/с</span>
-          <span>Влажность: {Math.round(currentWeather.main.humidity)}%</span>
+          <div className={styles.windAndHumidityItem}>
+            <img
+              src="/img/wind.png"
+              alt="wind-img"
+              className={styles.weekInfoImg}
+            />
+            <span>
+              {currentWeather.wind.speed.toFixed(1)} м/с
+            </span>
+          </div>
+          <div className={styles.windAndHumidityItem}>
+            <img
+              src="/img/humidity.png"
+              alt="humidity-img"
+              className={styles.weekInfoImg}
+            />
+            <span>{Math.round(currentWeather.main.humidity)}%</span>
+          </div>
         </div>
       </div>
     </div>
